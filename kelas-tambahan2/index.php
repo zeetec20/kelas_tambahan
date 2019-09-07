30e978539d6510a783e81748a2860a4f0c07cda6
Firman363 <jusles363@gmail.com>
2019-09-07 13:21:17 +0700

    uploud

---
+++ kelas-tambahan2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form class="form">
+        <h1 class="titleForm">LOGIN</h1>
+        <div class="inputName input">
+            <label for="username" class="labelName">Nama : </label>
+            <br>
+            <input type="text" name="username" id="">
+        </div>
+
+        <div class="inputEmail input">
+            <label for="email">Email : </label>
+            <br>
+            <input type="email" name="email" id="">
+        </div>
+
+        <div class="inputPassword input">
+            <label for="password">Password : </label>
+            <br>
+            <input type="password" name="password" id="">
+        </div>
+
+        <div class="button">
+            <button type="reset" class="reset">Batal</button>
+            <button type="submit" class="submit">Kirim</button>
+        </div>
+    </form>
+</body>
+</html>
